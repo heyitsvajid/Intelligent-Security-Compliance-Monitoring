2a7447c80399be90b315f43125e0565433ee0abe
Vajid Kagdi <vajid9@gmail.com>
2019-10-15 01:03:43 -0700

    Intial design for home page of complaince report

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import { compose } from 'recompose';
+import RuleTable from '../Rule/RuleTable'
+import RuleBarChart from '../Rule/RuleBarChart'
 
 import { AuthUserContext, withAuthorization, withEmailVerification } from '../Session';
+import Fade from 'react-reveal/Fade';
+
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Container,
+  Label,
+  Row
+} from "reactstrap";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faPlusSquare,
+    faMinusSquare
+  } from "@fortawesome/free-regular-svg-icons";
 
 const HomePage = (match) => (
   <AuthUserContext.Consumer>
@@ -31,17 +50,70 @@ class ComplainceReport extends React.Component {
   constructor(props) {
     super(props);
     let authUser = this.props.authUser
-    console.log(authUser)
 
     this.state = {
+      "s3": false,
       "name": authUser ? authUser.name : "",
       "email": authUser ? authUser.email : "",
     };
+    this.openService = this.openService.bind(this)
   }
+
+  openService(id) {
+    switch(id){
+        case 1:
+            this.setState({
+                s3:!this.state.s3
+            }) 
+        break    
+    }
+}
 
   render = () => {
     return (
-      <div>Add Complaince Report</div>
+              <Container fluid className="p-0">
+              <Row className="mt-3">
+              <Col md="1"/>
+                <Col md="10">
+                <Card>
+                    <CardHeader>
+                    <Row className="mt-3">
+                      <Col style = {{textAlign: 'center'}}> 
+                          <h1 style = {{color: '#2698da '}}>Complaince Report</h1> 
+                      </Col>
+                      </Row>
+                    </CardHeader>
+                  <CardBody>
+                  <div>
+                  <Row onClick={this.openService.bind(this,1)}>
+                      <Col md="11"><h4>Simple Storage Service (S3)</h4></Col>
+                      <Col md="1"><FontAwesomeIcon size="lg" icon={this.state.s3 ? faMinusSquare: faPlusSquare}/></Col>
+                  </Row>
+                  <Fade>
+                  <div  className="mt-3" style={{display: this.state.s3 ? 'block' : 'none',transition: 'display 1s'}}>
+                    <Row>
+                      <Col md="5">
+                        <RuleBarChart 
+                        chartData = {[
+                          {
+                            name: 'Buckets', passed: 2, failed: 1
+                          }
+                        ]}/>
+                      </Col>
+                      <Col md="7">
+                      <RuleTable headers = {["Test1", "Test2"]} rows = {[["Test1", "Test2"]]} title = {"Test1"} />
+                      </Col>
+                    </Row>
+                  </div>
+                  </Fade>
+                  <hr/>
+                  </div>         
+          </CardBody>
+        </Card>
+                  </Col>
+                <Col md="1"/>
+              </Row>
+            </Container>
     );
   }
 }
