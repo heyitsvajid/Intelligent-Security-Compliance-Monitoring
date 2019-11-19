c10b15eeba897515e796178fb74803419f2166d5
Vajid Kagdi <vajid9@gmail.com>
2019-11-19 00:37:04 -0800

    Update s3controller.js

--- backend/controllers/s3controller.js
+++ backend/controllers/s3controller.js
@@ -42,24 +42,25 @@ exports.s3FullControlAccess = function(req, res) {
                 return
             }
         log.info("All Bucket Acl: " + JSON.stringify(bucketAcl));
-        let failedBucketsName = [];
-        let passedBucketsName = [];
+        let failed = [];
+        let passed = [];
         for(let i = 0; i<bucketAcl.length; i++){
             let ownerId = bucketAcl[i]["Owner"]["ID"]
             let grants = bucketAcl[i]["Grants"]
             for(let j = 0; j<grants.length; j++){
                 if(grants[j]["Grantee"]["ID"] !== ownerId && grants[j]["Permission"] === 'FULL_CONTROL'){
-                    failedBucketsName.push(bucketAcl[i].Name)
+                    failed.push(bucketAcl[i].Name)
                 }
             }
-            if(!failedBucketsName.includes(bucketAcl[i].Name))
-                passedBucketsName.push(bucketAcl[i].Name)
+            if(!failed.includes(bucketAcl[i].Name))
+                passed.push(bucketAcl[i].Name)
 
         }
         resultObject.success = true
         let data = {
-            failedBucketsName,
-            passedBucketsName
+            bucketList,
+            failed,
+            passed
          }
          resultObject.data = data
         res.status(200).json(resultObject);
@@ -106,10 +107,25 @@ exports.s3BucketEncryption = function(req, res) {
                 res.status(400).json(resultObject);
                 return
             }
-        log.info("Buckets Encryption: " + JSON.stringify(bucketEncryption));
+            log.info("Buckets Encryption: " + JSON.stringify(bucketEncryption));
+            let passed = []
+            let failed = []
+            for(let i = 0; i<bucketEncryption.length; i++){
+                let bucketNames =  Object.keys(bucketEncryption[i])
+                log.info("Bucket Names: " + JSON.stringify(bucketNames));
+                if(bucketNames.length > 0){
+                    if(bucketEncryption[i][bucketNames[0]]){
+                        passed.push(bucketNames[0])
+                    }else{
+                        failed.push(bucketNames[0])
+                    }
+                }
+            }    
         resultObject.success = true
         let data = {
-            bucketEncryption:bucketEncryption
+            bucketList,
+            passed,
+            failed
         }
         resultObject.data = data
         res.status(200).json(resultObject);
@@ -221,6 +237,194 @@ exports.s3PublicAccess = function(req, res) {
         }
         resultObject.success = true
         let data = {
+            failedBucketsName,
+            passedBucketsName
+         }
+         resultObject.data = data
+        res.status(200).json(resultObject);
+    })
+    })    
+}
+
+/**
+ * Servive:S3
+ * Rule: S3 Buckets Encrypted with Customer-Provided CMKs.
+ * 
+ * @param accountId
+ * @param accountKey 
+ * 
+ * @returns List of failed and success bucket names for this rule.
+ */
+exports.s3BucketCustomerEncryption = function(req, res) {
+    let log = logger.getLogger(fileName + 's3BucketEncryption API')
+    log.info("Started: ")
+    log.info("Request Data: " + JSON.stringify(req.body))
+    let resultObject = new Model.ResultObject();
+
+    const creds = new AWS.Credentials({
+    accessKeyId: process.env.AWSAccessKeyId, secretAccessKey: process.env.AWSSecretKey, sessionToken: null
+    });
+
+    S3Service.getAllS3BucketsList(creds, function(err, bucketList) {
+        if (err) {
+            log.error("Error Calling s3Service.getAllS3BucketsList: " + JSON.stringify(err));
+            resultObject.success = false
+            resultObject.errorMessage = err.message
+            res.status(400).json(resultObject);
+            return
+        }
+        log.info("All Bucket List: " + JSON.stringify(bucketList));
+        
+        S3Service.getAllS3BucketsCustomerEncryption(creds,bucketList["Buckets"], (err, bucketEncryption)=>{
+            if (err) {
+                log.error("Error Calling s3Service.getAllS3BucketsEncryption: " + JSON.stringify(err));
+                resultObject.success = false
+                resultObject.errorMessage = err.message
+                res.status(400).json(resultObject);
+                return
+            }
+        log.info("Buckets Encryption: " + JSON.stringify(bucketEncryption));
+        let failedBucketsName = [];
+        let passedBucketsName = [];
+
+        for(let i = 0; i< bucketEncryption.length; i++){
+
+            let bucketEncryptionInfo = bucketEncryption[i]
+            let keys = Object.keys(bucketEncryptionInfo)
+            let bucketName = keys[0]
+            if(JSON.stringify(bucketEncryptionInfo).includes("AES256")){
+                failedBucketsName.push(bucketName)
+            }else{
+                passedBucketsName.push(bucketName)
+            }
+        }
+
+
+        resultObject.success = true
+        let data = {
+            failedBucketsName,
+            passedBucketsName
+        }
+        resultObject.data = data
+        res.status(200).json(resultObject);
+    
+        })
+    })    
+}
+
+
+/**
+ * Servive:S3
+ * Rule: Limit S3 Bucket Access by IP Address.
+ * 
+ * @param accountId
+ * @param accountKey 
+ * 
+ * @returns List of failed and success bucket names for this rule..
+ */
+exports.s3LimitByIpAccess = function(req, res) {
+    let log = logger.getLogger(fileName + 's3PublicAccess API')
+    log.info("Started: ")
+    log.info("Request Data: " + JSON.stringify(req.body))
+    let resultObject = new Model.ResultObject();
+
+    const creds = new AWS.Credentials({
+    accessKeyId: process.env.AWSAccessKeyId, secretAccessKey: process.env.AWSSecretKey, sessionToken: null
+    });
+
+    S3Service.getAllS3BucketsList(creds, function(err, bucketList) {
+        if (err) {
+            log.error("Error Calling s3Service.getAllS3BucketsList: " + JSON.stringify(err));
+            resultObject.success = false
+            resultObject.errorMessage = err.message
+            res.status(400).json(resultObject);
+            return
+        }
+        log.info("All Bucket List: " + JSON.stringify(bucketList));
+        
+        S3Service.getAllS3BucketsPolicy(creds,bucketList["Buckets"], (err, bucketPolicies)=>{
+            if (err) {
+                log.error("Error Calling s3Service.getAllS3BucketsPolicy: " + JSON.stringify(err));
+                resultObject.success = false
+                resultObject.errorMessage = err.message
+                res.status(400).json(resultObject);
+                return
+            }
+        log.info("All Bucket Acl: " + JSON.stringify(bucketPolicies));
+        let failedBucketsName = [];
+        let passedBucketsName = [];
+        for(let i = 0; i<bucketPolicies.length; i++){
+
+            if(JSON.stringify(bucketPolicies[i]).includes("NoSuchBucketPolicy")){
+                failedBucketsName.push(Object.keys(bucketPolicies[i])[0])
+            }else{
+                passedBucketsName.push(Object.keys(bucketPolicies[i])[0])
+            }
+
+        }
+        resultObject.success = true
+        let data = {
+            failedBucketsName,
+            passedBucketsName
+         }
+         resultObject.data = data
+        res.status(200).json(resultObject);
+    })
+    })    
+}
+
+
+/**
+ * Servive:S3
+ * Rule: Ensure S3 bucket access logging is enabled on the CloudTrail S3.
+ * 
+ * @param accountId
+ * @param accountKey 
+ * 
+ * @returns List of failed and success bucket names for this rule..
+ */
+exports.s3BucketLogging = function(req, res) {
+    let log = logger.getLogger(fileName + 's3BucketLogging API')
+    log.info("Started: ")
+    log.info("Request Data: " + JSON.stringify(req.body))
+    let resultObject = new Model.ResultObject();
+
+    const creds = new AWS.Credentials({
+    accessKeyId: process.env.AWSAccessKeyId, secretAccessKey: process.env.AWSSecretKey, sessionToken: null
+    });
+
+    S3Service.getAllS3BucketsList(creds, function(err, bucketList) {
+        if (err) {
+            log.error("Error Calling s3Service.getAllS3BucketsList: " + JSON.stringify(err));
+            resultObject.success = false
+            resultObject.errorMessage = err.message
+            res.status(400).json(resultObject);
+            return
+        }
+        log.info("All Bucket List: " + JSON.stringify(bucketList));
+        
+        S3Service.getAllS3BucketsLogging(creds,bucketList["Buckets"], (err, bucketLogging)=>{
+            if (err) {
+                log.error("Error Calling s3Service.getAllS3BucketsLogging: " + JSON.stringify(err));
+                resultObject.success = false
+                resultObject.errorMessage = err.message
+                res.status(400).json(resultObject);
+                return
+            }
+        log.info("All Bucket Logging: " + JSON.stringify(bucketLogging));
+        let failedBucketsName = [];
+        let passedBucketsName = [];
+        for(let i = 0; i<bucketLogging.length; i++){
+
+            if(!JSON.stringify(bucketLogging[i]).includes("LoggingEnabled")){
+                failedBucketsName.push(Object.keys(bucketLogging[i])[0])
+            }else{
+                passedBucketsName.push(Object.keys(bucketLogging[i])[0])
+            }
+        }
+        resultObject.success = true
+        let data = {
+            bucketLogging,
             failedBucketsName,
             passedBucketsName
          }
