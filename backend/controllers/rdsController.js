3bfe0651f5f1067f5dffa3350b8a62b7505449ab
Vajid Kagdi <vajid9@gmail.com>
2019-11-19 00:36:55 -0800

    RDS rules added

---
+++ backend/controllers/rdsController.js
@@ -0,0 +1,253 @@
+const logger = require('../config/logger');
+const Model = require('../model/resultObject.js');
+const RDSService = require('../utility/rdsService');
+const fileName = "rdsController: ";
+const AWS = require('aws-sdk')
+
+/**
+ * Servive:RDS
+ * Rule: Enable AWS RDS Automated Backups.
+ * 
+ * @param accountId
+ * @param accountKey 
+ * 
+ * @returns List of failed and success bucket names for this rule..
+ */
+exports.rdsAutomatedBackup = function(req, res) {
+    let log = logger.getLogger(fileName + 'rdsAutomatedBackup API')
+    log.info("Started: ")
+    log.info("Request Data: " + JSON.stringify(req.body))
+    let resultObject = new Model.ResultObject();
+
+    const creds = new AWS.Credentials({
+    accessKeyId: process.env.AWSAccessKeyId, secretAccessKey: process.env.AWSSecretKey, sessionToken: null
+    });
+
+    RDSService.getAllRDSInstanceList(creds, function(err, rdsList) {
+        if (err) {
+            log.error("Error Calling RDSService.getAllRDSInstanceList: " + JSON.stringify(err));
+            resultObject.success = false
+            resultObject.errorMessage = err.message
+            res.status(400).json(resultObject);
+            return
+        }
+        log.info("All RDS List: " + JSON.stringify(rdsList));        
+        let failedRDSName = [];
+        let passedRDSName = [];
+        let dbIdentifiers = Object.keys(rdsList) 
+        for(let i = 0; i<dbIdentifiers.length; i++){
+            let instanceDescription = rdsList[dbIdentifiers[i]]
+            if(instanceDescription.BackupRetentionPeriod <= 0){
+                failedRDSName.push(dbIdentifiers[i])
+            }else{
+                passedRDSName.push(dbIdentifiers[i])
+            }
+        }
+        resultObject.success = true
+        let data = {
+            passedRDSName,
+            failedRDSName
+         }
+         resultObject.data = data
+        res.status(200).json(resultObject);
+    })   
+     
+}
+
+
+/**
+ * Servive:RDS
+ * Rule: Enable AWS RDS Deletion Protection.
+ * 
+ * @param accountId
+ * @param accountKey 
+ * 
+ * @returns List of failed and success bucket names for this rule..
+ */
+exports.rdsDeletionProtection = function(req, res) {
+    let log = logger.getLogger(fileName + 'rdsDeletionProtection API')
+    log.info("Started: ")
+    log.info("Request Data: " + JSON.stringify(req.body))
+    let resultObject = new Model.ResultObject();
+
+    const creds = new AWS.Credentials({
+    accessKeyId: process.env.AWSAccessKeyId, secretAccessKey: process.env.AWSSecretKey, sessionToken: null
+    });
+
+    RDSService.getAllRDSInstanceList(creds, function(err, rdsList) {
+        if (err) {
+            log.error("Error Calling RDSService.getAllRDSInstanceList: " + JSON.stringify(err));
+            resultObject.success = false
+            resultObject.errorMessage = err.message
+            res.status(400).json(resultObject);
+            return
+        }
+        log.info("All RDS List: " + JSON.stringify(rdsList));        
+        let failedRDSName = [];
+        let passedRDSName = [];
+        let dbIdentifiers = Object.keys(rdsList) 
+        for(let i = 0; i<dbIdentifiers.length; i++){
+            let instanceDescription = rdsList[dbIdentifiers[i]]
+            if(instanceDescription.DeletionProtection){
+                passedRDSName.push(dbIdentifiers[i])
+            }else{
+                failedRDSName.push(dbIdentifiers[i])
+            }
+        }
+        resultObject.success = true
+        let data = {
+            passedRDSName,
+            failedRDSName
+         }
+         resultObject.data = data
+        res.status(200).json(resultObject);
+    })     
+}
+
+/**
+ * Servive:RDS
+ * Rule: Enable AWS RDS Encryption.
+ * 
+ * @param accountId
+ * @param accountKey 
+ * 
+ * @returns List of failed and success bucket names for this rule..
+ */
+exports.rdsEncryption = function(req, res) {
+    let log = logger.getLogger(fileName + 'rdsEncryption API')
+    log.info("Started: ")
+    log.info("Request Data: " + JSON.stringify(req.body))
+    let resultObject = new Model.ResultObject();
+
+    const creds = new AWS.Credentials({
+    accessKeyId: process.env.AWSAccessKeyId, secretAccessKey: process.env.AWSSecretKey, sessionToken: null
+    });
+
+    RDSService.getAllRDSInstanceList(creds, function(err, rdsList) {
+        if (err) {
+            log.error("Error Calling RDSService.getAllRDSInstanceList: " + JSON.stringify(err));
+            resultObject.success = false
+            resultObject.errorMessage = err.message
+            res.status(400).json(resultObject);
+            return
+        }
+        log.info("All RDS List: " + JSON.stringify(rdsList));        
+        let failedRDSName = [];
+        let passedRDSName = [];
+        let dbIdentifiers = Object.keys(rdsList) 
+        for(let i = 0; i<dbIdentifiers.length; i++){
+            let instanceDescription = rdsList[dbIdentifiers[i]]
+            if(instanceDescription.StorageEncrypted){
+                passedRDSName.push(dbIdentifiers[i])
+            }else{
+                failedRDSName.push(dbIdentifiers[i])
+            }
+        }
+        resultObject.success = true
+        let data = {
+            passedRDSName,
+            failedRDSName
+         }
+         resultObject.data = data
+        res.status(200).json(resultObject);
+    })     
+}
+
+/**
+ * Servive:RDS
+ * Rule: Enable AWS RDS Encryption.
+ * 
+ * @param accountId
+ * @param accountKey 
+ * 
+ * @returns List of failed and success bucket names for this rule..
+ */
+exports.rdsEncryption = function(req, res) {
+    let log = logger.getLogger(fileName + 'rdsEncryption API')
+    log.info("Started: ")
+    log.info("Request Data: " + JSON.stringify(req.body))
+    let resultObject = new Model.ResultObject();
+
+    const creds = new AWS.Credentials({
+    accessKeyId: process.env.AWSAccessKeyId, secretAccessKey: process.env.AWSSecretKey, sessionToken: null
+    });
+
+    RDSService.getAllRDSInstanceList(creds, function(err, rdsList) {
+        if (err) {
+            log.error("Error Calling RDSService.getAllRDSInstanceList: " + JSON.stringify(err));
+            resultObject.success = false
+            resultObject.errorMessage = err.message
+            res.status(400).json(resultObject);
+            return
+        }
+        log.info("All RDS List: " + JSON.stringify(rdsList));        
+        let failedRDSName = [];
+        let passedRDSName = [];
+        let dbIdentifiers = Object.keys(rdsList) 
+        for(let i = 0; i<dbIdentifiers.length; i++){
+            let instanceDescription = rdsList[dbIdentifiers[i]]
+            if(instanceDescription.StorageEncrypted){
+                passedRDSName.push(dbIdentifiers[i])
+            }else{
+                failedRDSName.push(dbIdentifiers[i])
+            }
+        }
+        resultObject.success = true
+        let data = {
+            passedRDSName,
+            failedRDSName
+         }
+         resultObject.data = data
+        res.status(200).json(resultObject);
+    })     
+}
+
+/**
+ * Servive:RDS
+ * Rule: Enable IAM Database Authentication.
+ * 
+ * @param accountId
+ * @param accountKey 
+ * 
+ * @returns List of failed and success instances name for this rule.
+ */
+exports.rdsIAMAuthentication = function(req, res) {
+    let log = logger.getLogger(fileName + 'rdsEncryption API')
+    log.info("Started: ")
+    log.info("Request Data: " + JSON.stringify(req.body))
+    let resultObject = new Model.ResultObject();
+
+    const creds = new AWS.Credentials({
+    accessKeyId: process.env.AWSAccessKeyId, secretAccessKey: process.env.AWSSecretKey, sessionToken: null
+    });
+
+    RDSService.getAllRDSInstanceList(creds, function(err, rdsList) {
+        if (err) {
+            log.error("Error Calling RDSService.getAllRDSInstanceList: " + JSON.stringify(err));
+            resultObject.success = false
+            resultObject.errorMessage = err.message
+            res.status(400).json(resultObject);
+            return
+        }
+        log.info("All RDS List: " + JSON.stringify(rdsList));        
+        let failedRDSName = [];
+        let passedRDSName = [];
+        let dbIdentifiers = Object.keys(rdsList) 
+        for(let i = 0; i<dbIdentifiers.length; i++){
+            let instanceDescription = rdsList[dbIdentifiers[i]]
+            if(instanceDescription.IAMDatabaseAuthenticationEnabled){
+                passedRDSName.push(dbIdentifiers[i])
+            }else{
+                failedRDSName.push(dbIdentifiers[i])
+            }
+        }
+        resultObject.success = true
+        let data = {
+            rdsList,
+            passedRDSName,
+            failedRDSName
+         }
+         resultObject.data = data
+        res.status(200).json(resultObject);
+    })     
+}
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,8 @@
 module.exports = function (app) {
 
-  var controller = require('../controllers/controller.js');
-  var s3Ccontroller = require('../controllers/s3controller.js');
+  const controller = require('../controllers/controller.js');
+  const s3Ccontroller = require('../controllers/s3controller.js');
+  const rdsCcontroller = require('../controllers/rdsController.js');
 
   //Ping Server
   app.get('/ping', controller.ping);
@@ -13,17 +14,35 @@ module.exports = function (app) {
   app.post('/underutilizedInstances', controller.underutilizedInstances);
 
   // AWS S3 Bucket Public 'FULL_CONTROL' Access
-    app.post('/s3FullControlAccess', s3Ccontroller.s3FullControlAccess);
+  app.post('/s3FullControlAccess', s3Ccontroller.s3FullControlAccess);
 
   // AWS S3 Bucket Encryption
   app.post('/s3BucketEncryption', s3Ccontroller.s3BucketEncryption);
 
   // AWS S3 Bucket MFA Delete
   app.post('/s3BucketMfaDelete', s3Ccontroller.s3BucketMfaDelete);
-  
-  // AWS S3 Bucket MFA Delete
+
+  // AWS S3 Bucket Public Access
   app.post('/s3PublicAccess', s3Ccontroller.s3PublicAccess);
-  
-  }
 
+  // AWS S3 Bucket Bucket Customer Encryption
+  app.post('/s3BucketCustomerEncryption', s3Ccontroller.s3BucketCustomerEncryption);
 
+  // AWS S3 Bucket Public Access
+  app.post('/s3LimitByIpAccess', s3Ccontroller.s3LimitByIpAccess);
+
+  // AWS S3 Bucket Logging
+  app.post('/s3BucketLogging', s3Ccontroller.s3BucketLogging);
+
+  // AWS RDS Automated Backup
+  app.post('/rdsAutomatedBackup', rdsCcontroller.rdsAutomatedBackup);
+
+  // AWS RDS Deletion Protection
+  app.post('/rdsDeletionProtection', rdsCcontroller.rdsDeletionProtection);
+
+  // AWS RDS Encryption
+  app.post('/rdsEncryption', rdsCcontroller.rdsEncryption);
+
+  // AWS RDS IAMAuthentication
+  app.post('/rdsIAMAuthentication', rdsCcontroller.rdsIAMAuthentication);
+}
